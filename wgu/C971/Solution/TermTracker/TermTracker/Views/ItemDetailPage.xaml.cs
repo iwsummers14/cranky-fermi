20d4b9dd85534d252de6d6cdcaa5ea9fa3a91d2f
Ian Summers <iwsummers14@outlook.com>
2020-08-26 22:15:46 -0500

    initial solution and wireframe added

---
+++ wgu/C971/Solution/TermTracker/TermTracker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TermTracker.ViewModels;
+
+namespace TermTracker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
